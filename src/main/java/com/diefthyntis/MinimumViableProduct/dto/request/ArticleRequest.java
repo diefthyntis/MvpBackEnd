1d9d7b3e644219cf9b855ad867c902f220e39a8f
Alan Hauser <diefthyntis@gmail.com>
2024-09-20 09:16:53 +0200

    Commit initial

---
+++ src/main/java/com/diefthyntis/MinimumViableProduct/dto/request/ArticleRequest.java
@@ -0,0 +1,11 @@
+package com.diefthyntis.MinimumViableProduct.dto.request;
+
+import lombok.Data;
+
+@Data
+public class ArticleRequest {
+	private String sentence;
+	private String speakerid;
+	private String topicid;
+	private String title;
+}
---
+++ src/main/java/com/diefthyntis/MinimumViableProduct/dto/request/SubscriptionRequest.java
@@ -0,0 +1,9 @@
+package com.diefthyntis.MinimumViableProduct.dto.request;
+
+import lombok.Data;
+
+@Data
+public class SubscriptionRequest {
+	private String speakerid;
+	private String topicid;
+}
